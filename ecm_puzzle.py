9affdb0d9419324d059ba8b696b552c42cf59358
jbrownrs <36203116+jbrownrs@users.noreply.github.com>
2018-07-11 23:05:54 +0100

    Clarifies output text

--- ecm_puzzle.py
+++ ecm_puzzle.py
@@ -19,37 +19,38 @@ What is the capacity of the jug?
 import random
 from math import log10, floor, sqrt
 
-punchbowl = float(input("How big is your punchbowl?"))
-# initialise with a random jug capacity guess
-jug = random.uniform(0.01, punchbowl-0.01)
-print(jug)
-
-#howManyRepetitions = int((input("How many times do you wish to use the jug?"))
-
-def round_sig(x, sig=5):
+def round_sig(x, sig):
    return round(x, sig-int(floor(log10(abs(x))))-1)
 
 def dip_the_bowl(punchbowl, jug):
-    #for _ in xrange(howManyRepetitions):
+    # for _ in xrange(howManyRepetitions):
+    # currently defaults to two dips to remove punch and add water
     punch_amount_reduced = punchbowl - jug
-    #how much punch is there if we then add in the jug of water
+    # how much punch is there if we then add in the jug of water
     punch_proportion = punch_amount_reduced/punchbowl
     # the actual punch amount will be the same as before addition of water
     # only the proportion will change
     # second removal of punch
     punch_amount_reduced_2 = (punch_amount_reduced)*punch_proportion
-    #punch_amount_two = ((punchbowl - jug)/punchbowl)*(punchbowl - jug)
     # add water for second time what's the proportion of punch
     punch_proportion_two = punch_amount_reduced_2/punchbowl
-    print('Proportion of the punchbowl that is punch is', punch_proportion_two,
-          'given a jug of capacity', jug)    
-    if round_sig(punch_proportion_two) == 0.50:
-        print('For a 50:50 mix (aka', punch_proportion_two,
-        '% punch) the jug capacity is', jug)   
+    # print('Proportion of the punchbowl that is punch is', punch_proportion_two,
+    # 'given a jug of capacity', jug)    
+    if round_sig(punch_proportion_two,5) == 0.50:
+        print('For a 1:1 mix (aka',round_sig(punch_proportion_two,2)*100,
+        '% punch) the jug capacity is', round_sig(jug,3),'litres (to 3 s.f.)')   
         return
     else:
         # if not enough punch we need a smaller jug
         # if too much punch we need a bigger jug
         jug = jug + (jug*(punch_proportion_two - 0.5))
         dip_the_bowl(punchbowl,jug)
-            
+
+
+punchbowl = float(input("How big is your punchbowl (in litres)?"))
+# initialise with a random jug capacity guess
+jug = random.uniform(0.01, punchbowl-0.01)
+print("Random initial jug capacity guess:",round_sig(jug,3),'litres')
+#howManyRepetitions = int((input("How many times do you wish to use the jug?"))
+dip_the_bowl(punchbowl, jug)
+
